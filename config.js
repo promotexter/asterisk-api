7fa43359ebf89de4a61b03a0b252e10c8e2bf73e
Erwin Atuli <erwin@promotexter.com>
2015-09-12 20:59:48 -0400

    changes to restart

--- config.js
+++ config.js
@@ -3,16 +3,22 @@ module.exports = {
 	asterisk : {
 		extension_conf : {
 			path : '/etc/asterisk/extensions.conf',
-			template : "exten => %username%,1,Dial(SIP/%username%, 15)",
-			
+			template : "exten => %username%,1,Dial(SIP/%username%, 15, r)",
 			header: "[internal]"
 		},
 		sip_conf : {
 			path : '/etc/asterisk/sip.conf',
 			template : "[%username%]\ntype=friend\nhost=dynamic\nsecret=%username%\ncontext=internal",
-			header : ""
+			header: "[general]\ndisallow=all\nallow=ulaw\nallow=gsm\nprematuremedia=no\nprogressinband=yes\nsilencesuppression=no"
 			
+		},
+		manager: {
+			host: '127.0.0.1',
+			port: 5038,
+			username: 'erwin',
+			password: 'atuli'
 		}
+		
 	},
 	mongo : {
 		uri : 'mongodb://localhost/test'
